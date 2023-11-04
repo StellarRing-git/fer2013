22e4d08b22ea0e8d3bc5f912045d5fc0a451f43d
StellarRing-git <149808896+StellarRing-git@users.noreply.github.com>
2023-11-04 19:53:57 +0530

    Update fertestcustom.py

--- fertestcustom.py
+++ fertestcustom.py
@@ -6,7 +6,7 @@ from keras.models import model_from_json
 import numpy
 import os
 import numpy as np
-import cv2
+import cv2 as cv
 
 #loading the model
 json_file = open('fer.json', 'r')
@@ -22,25 +22,35 @@ WIDTH = 48
 HEIGHT = 48
 x=None
 y=None
-labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+z='placeholder'
+labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Suprise', 'Neutral']
 
-#loading image
-full_size_image = cv2.imread("test.jpg")
-print("Image Loaded")
-gray=cv2.cvtColor(full_size_image,cv2.COLOR_RGB2GRAY)
-face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-faces = face.detectMultiScale(gray, 1.3  , 10)
+#taking video
+cap = cv.VideoCapture(0)
 
-#detecting faces
-for (x, y, w, h) in faces:
-        roi_gray = gray[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
-        cv2.rectangle(full_size_image, (x, y), (x + w, y + h), (0, 255, 0), 1)
-        #predicting the emotion
-        yhat= loaded_model.predict(cropped_img)
-        cv2.putText(full_size_image, labels[int(np.argmax(yhat))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
-        print("Emotion: "+labels[int(np.argmax(yhat))])
+while True:
+        ret, frame = cap.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        face = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+        faces = face.detectMultiScale(gray, 1.3  , 10)
+        #detecting faces
+        for (x, y, w, h) in faces:
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv.resize(roi_gray, (48, 48)), -1), 0)
+                cv.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv.NORM_L2, dtype=cv.CV_32F)
+                cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+                #predicting the emotion
+                yhat= loaded_model.predict(cropped_img)
+                cv.putText(frame, labels[int(np.argmax(yhat))], (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv.LINE_AA)
+                emotion=labels[int(np.argmax(yhat))]
+                if emotion!=z:
+                        print("Emotion: "+labels[int(np.argmax(yhat))])
+                        z=emotion
 
-cv2.imshow('Emotion', full_size_image)
-cv2.waitKey()
+                cv.imshow('camera',frame)
+
+        if cv.waitKey(1) == ord('q'):
+                break
+
+cap.release()
+cv.destroyAllWindows()
